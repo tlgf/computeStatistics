655746d0b9314f89d853301380ae9a935f82abc7
tomf <tom@flynn-labs.com>
2023-12-30 17:46:43 +1100

    Hacked up build script

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ rm -rf build
 mkdir build
 set -e
 pushd ./build
-cmake ..
-time make
+#cmake -DCMAKE_INCLUDE_PATH=/home/tom/prog/_libs/include/boost/ -DBoost_NO_BOOST_CMAKE=TRUE -DBOOST_ROOT=/home/tom/prog/_libs/ ..
+cmake -DBoost_NO_BOOST_CMAKE=TRUE -DBOOST_ROOT=/home/tom/prog/_libs/ ..
+time make VERBOSE=1
 popd
