e2447a2549011ec30fb70a28cf436c26e0d83c11
tomf <tom@flynn-labs.com>
2023-12-31 15:00:38 +1100

    Cleanup the helper script now conan is available

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-rm -rf build
-mkdir build
-set -e
-pushd ./build
-#cmake -DCMAKE_INCLUDE_PATH=/home/tom/prog/_libs/include/boost/ -DBoost_NO_BOOST_CMAKE=TRUE -DBOOST_ROOT=/home/tom/prog/_libs/ ..
-cmake -DBoost_NO_BOOST_CMAKE=TRUE -DBOOST_ROOT=/home/tom/prog/_libs/ ..
-time make VERBOSE=1
-popd
